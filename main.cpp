d2b368750f14e8a242276576ffd972285a24792b
vinso <8vinso8@gmail.com>
2022-03-29 12:06:54 +1000

    Теперь не нужно вводить количество строк

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cppcoreguidelines-narrowing-conversions"
+// clazy:skip
 #include <QApplication>
 #include <QHBoxLayout>
 #include <QPushButton>
 #include <fstream>
+#include <vector>
 using namespace std;
 int main(int argc, char *argv[]){
     ifstream fin("sequence.txt");
     int n;
-    fin >> n;
-    auto *sequence = new string[n];
+    vector <string> sequence;
+    string line;
+    while(getline(fin,line)){
+        sequence.push_back(line);
+    }
+    n = sequence.size();
     for (int i = 0; i < n; i++){ fin >> sequence[i]; }
     fin.close();
     QApplication app(argc,argv);
@@ -19,7 +27,7 @@ int main(int argc, char *argv[]){
             QString qbnum = QString::fromStdString(bnum);
             auto* button = new QPushButton(qbnum, &window);
             button->setGeometry(31 * i, 31 * j, 30, 30);
-            QObject::connect(button, &QPushButton::clicked, [i, button, j, sequence](){
+            QObject::connect(button, &QPushButton::clicked, [i, button, j, &sequence](){
                 string a = sequence[j][i] == '0' ? "1" : "0";
                 sequence[j][i] = a[0];
                 button->setText(QString::fromStdString(a));
@@ -27,13 +35,13 @@ int main(int argc, char *argv[]){
         }
     }
     auto *save = new QPushButton("Сохранить", &window);
-    save->setGeometry(0, 30 * n, 80, 30);
-    QObject::connect(save, &QPushButton::clicked, [n, sequence](){
+    save->setGeometry(0, 31 * n, 80, 30);
+    QObject::connect(save, &QPushButton::clicked, [n, &sequence](){
         ofstream fout("sequence.txt");
-        fout << n << endl;
         for (int i = 0; i < n; i++){ fout << sequence[i] << endl; }
         fout.close();
     });
     window.show();
     return QApplication::exec();
 }
+#pragma clang diagnostic pop
